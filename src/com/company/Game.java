4c3a6260af868f458b89d52ce75a59ae7169f538
Stratfab <fabsiegele@gmail.com>
2020-11-05 15:10:55 +0100

    classes + menu

---
+++ src/com/company/Game.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Game {
+
+    public static void play(){
+
+        System.out.println("Welcome to Dungeon" );
+
+
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+            boolean keepPlaying = true;
+
+            while (keepPlaying) {
+                System.out.println("choose between following options");
+                System.out.println("1 - Start Game");
+                System.out.println("2 - Quit");
+
+
+                int userInput = getUserInput();
+                switch (userInput) {
+                    case 1 -> {
+                        Game.play();
+                    }
+
+                    case 2 -> {
+                        return;
+                    }
+                    default -> {
+                        System.out.println("Option not available, please try again");
+                    }
+                }
+            }
+        }
+
+        public static int getUserInput () {
+            Scanner sc = new Scanner(System.in);
+
+            int userInput = 0;
+            try {
+                userInput = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Sorry wrong input, please try again");
+            }
+            sc.nextLine();
+            return userInput;
+        }
     }
-}
